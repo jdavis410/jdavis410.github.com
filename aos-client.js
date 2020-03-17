a6e7fedfe4d798ea563b3edb45aabcde34994b78
Joshua P. Davis <josh95322@gmail.com>
2020-03-17 16:18:32 -0400

    added response obj

--- aos-client.js
+++ aos-client.js
@@ -1,4 +1,5 @@
 (function (global) {
+  var Response;
   function isReactAppReady() {
     var response = null;
     if (!window.ReactNativeWebView) {
@@ -8,7 +9,8 @@
     try {
       window.ReactNativeWebView.postMessage(COMMAND.checkInit);
       window.addEventListener("message", message => {
-        response = message;
+        response = message.data;
+        Response = message.data;
         console.log(message);
       });
       if (response == null) {
@@ -26,6 +28,7 @@
   var aos = {
     VERSION: '0.0.1',
     isReady: function() { return isReactAppReady()},
+    Response: Response
   };
 
   if (global.aos) {
